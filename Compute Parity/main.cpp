76249f0a86f6b72091cfa8f51ab85dcb628142ff
csearcy <christopher.m.searcy@gmail.com>
2015-07-07 17:05:08 -0600

    Initial Commit

---
+++ Compute Parity/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Compute Parity
+//
+//  Created by Chris Searcy on 7/7/15.
+//  Copyright (c) 2015 Chris Searcy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
